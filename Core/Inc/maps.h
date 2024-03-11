f39d3e5c1bf86e27e62f213ca23ad6a0d8347102
Orestis <batsisorestis@gmail.com>
2024-03-11 20:13:28 +0200

    Minor changes
    Changed the name of the clutch min/max defines in the Maps.h header file to be consistent with Shifter code

--- Core/Inc/maps.h
+++ Core/Inc/maps.h
@@ -12,13 +12,13 @@
 
 #define CLUTCH_PADDLE_MAP_SIZE	2
 
-#define CLUTCH_PADDLE_MAX		104
-#define CLUTCH_PADDLE_MIN		-4
+#define CLUTCH_PADDLE_MAP_MAX		104
+#define CLUTCH_PADDLE_MAP_MIN		-4
 
 static const float rClutchMap[2][CLUTCH_PADDLE_MAP_SIZE] = {
 
-		/* In:  VrClutchPaddleRaw */	{		0.500		,  		3.000	 	},
-		/* Out: rClutchPaddle 	  */	{ CLUTCH_PADDLE_MIN ,  CLUTCH_PADDLE_MAX}
+		/* In:  VrClutchPaddleRaw */	{		0.500			,  			3.000	 	},
+		/* Out: rClutchPaddle 	  */	{ CLUTCH_PADDLE_MAP_MIN ,  CLUTCH_PADDLE_MAP_MAX}
 };
 
 #endif /* INC_MAPS_H_ */
